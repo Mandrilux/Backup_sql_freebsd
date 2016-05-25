cd3d8d36a98bf688ec1020ad8815f9b5c42fbb97
epitech2020 <baptiste.heraud@epitech.eu>
2016-05-25 13:43:47 +0200

    time

--- backup.sh
+++ backup.sh
@@ -9,19 +9,18 @@ MyHOST="localhost"
 MYSQL="/usr/local/bin/mysql"
 MYSQLDUMP="/usr/local/bin/mysqldump"
 GZIP="/usr/bin/gzip"
-SCP="/usr/bin/scp"
 TAIL="/usr/bin/tail"
 AWK="/usr/bin/awk"
-DF="/bin/df"
 
 $MYSQLDUMP -u $MyUSER -h $MyHOST -p$MyPASS account | $GZIP > /var/tmp/mysql_backups/account.gz
 $MYSQLDUMP -u $MyUSER -h $MyHOST -p$MyPASS common | $GZIP > /var/tmp/mysql_backups/common.gz
 $MYSQLDUMP -u $MyUSER -h $MyHOST -p$MyPASS player | $GZIP > /var/tmp/mysql_backups/player.gz
+$MYSQLDUMP -u $MyUSER -h $MyHOST -p$MyPASS site | $GZIP > /var/tmp/mysql_backups/site.gz
 
 sleep 3 
-cd /home/
-tar czvf file.tar.gz metin2
-mv file.tar.gz /var/tmp/mysql_backups/
+#cd /home/
+#tar czvf file.tar.gz metin2
+#mv file.tar.gz /var/tmp/mysql_backups/
 cd /home/sauvegarde/
 sleep 2
 find . -type f -name 'savemysql*.tar.bz' | sed -e 's/^.*[^0-9]\([0-9]*\).tar.bz/\1/g'| sort -rn | while read A
@@ -33,4 +32,5 @@ tar cvjf savemysql1.tar.bz /var/tmp/mysql_backups/
 sleep 5
 cd /var/tmp/mysql_backups/
 rm *.gz
-
+cd /home/sauvegarde
+echo "`date -u` [+] OK [Taille] `du -hs`" >> log.txt
